d149cf0ac3f31c348f695355285dfc557a3c7139
Peter Abah <peterabah.ob@gmail.com>
2021-05-09 22:17:12 +0100

    Create HumanPlayer class

---
+++ human_player.rb
@@ -0,0 +1,31 @@
+class HumanPlayer
+  def initialize
+    puts 'Enter your name'
+    @name = gets
+  end
+
+  def make_code
+    code = 0
+    loop do
+      puts 'Enter valid code'
+      code = gets
+      break if valid_code?(code)
+    end
+    code  
+  end
+
+  def guess
+    guess = 0
+    loop do
+      puts 'Guess a valid code'
+      guess = gets
+      break if valid_code?(guess)
+    end
+  end
+
+  private
+
+  def valid_code?(code)
+    /^[1-6]{4}$/ =~ code
+  end
+end
